d3a4a65f993fb9e7491ceae4e295167a7aecd2b6
wiligetsurvey <wili@getsurvey.id>
2023-09-22 07:28:21 +0800

    feat - data perolehan suara

--- src/app.service.ts
+++ src/app.service.ts
@@ -109,7 +109,6 @@ export class AppService {
     created_pemilih.save();
 
     for (const el of bodyTarget.id_dpr_level) {
-      console.log(el);
       const data_suara = {
         _id: new Types.ObjectId(),
         user_id: id_pemilih,
@@ -126,20 +125,174 @@ export class AppService {
   }
 
   async getSuara(query: any): Promise<any> {
-    const { id_dpr_level } = query;
+    const { id_dpr_level, id_kabupaten, dapil } = query;
     let filter: any = {};
 
     if (id_dpr_level) filter.id_dpr_level = id_dpr_level;
-    return this.suara.aggregate([
+    if (id_kabupaten) filter['pemilih.id_kabupaten'] = id_kabupaten;
+    if (dapil === '1') {
+      filter['pemilih.id_kabupaten'] = { $in: ['5271'] };
+    }
+    if (dapil === '2') {
+      filter['pemilih.id_kabupaten'] = { $in: ['5201', '5208'] };
+    }
+    if (dapil === '3') {
+      filter['pemilih.id_kecamatan'] = {
+        $in: [
+          '5203031',
+          '5203030',
+          '5203040',
+          '5203020',
+          '5203021',
+          '5203022',
+          '5203010',
+          '5203011',
+        ],
+      };
+    }
+    if (dapil === '4') {
+      filter['pemilih.id_kecamatan'] = {
+        $nin: [
+          '5203031',
+          '5203030',
+          '5203040',
+          '5203020',
+          '5203021',
+          '5203022',
+          '5203010',
+          '5203011',
+        ],
+      };
+    }
+    if (dapil === '5') {
+      filter['pemilih.id_kabupaten'] = { $in: ['5207', '5204'] };
+    }
+    if (dapil === '6') {
+      filter['pemilih.id_kabupaten'] = { $in: ['5206', '5272', '5205'] };
+    }
+    if (dapil === '7') {
+      filter['pemilih.id_kecamatan'] = {
+        $in: ['5202060', '5202061', '5202040', '5202050', '5202090', '5202091'],
+      };
+    }
+    if (dapil === '8') {
+      filter['pemilih.id_kecamatan'] = {
+        $nin: [
+          '5202060',
+          '5202061',
+          '5202040',
+          '5202050',
+          '5202090',
+          '5202091',
+        ],
+      };
+    }
+
+    const data = this.suara.aggregate([
       {
-        $match: filter,
+        $lookup: {
+          from: 'pemilihs',
+          localField: 'user_id',
+          foreignField: '_id',
+          as: 'pemilih',
+        },
+      },
+      {
+        $unwind: {
+          path: '$pemilih',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $match: { ...filter },
       },
       {
         $group: {
           _id: '$id_dpr_level',
-          count: { $sum: 1 }, 
+          anggota: {
+            $sum: {
+              $cond: [{ $ne: ['$pemilih.id_upa', ''] }, 1, 0],
+            },
+          },
+          dpc: {
+            $sum: {
+              $cond: [{ $eq: ['$pemilih.id_sub_category', '5'] }, 1, 0],
+            },
+          },
+          dpra: {
+            $sum: {
+              $cond: [{ $eq: ['$pemilih.id_sub_category', '6'] }, 1, 0],
+            },
+          },
+          bko: {
+            $sum: {
+              $cond: [{ $eq: ['$pemilih.id_category', '5'] }, 1, 0],
+            },
+          },
+          bpkk: {
+            $sum: {
+              $cond: [{ $eq: ['$pemilih.id_category', '4'] }, 1, 0],
+            },
+          },
+          bpu: {
+            $sum: {
+              $cond: [{ $eq: ['$pemilih.id_category', '7'] }, 1, 0],
+            },
+          },
+          kepemudaan: {
+            $sum: {
+              $cond: [{ $eq: ['$pemilih.id_category', '6'] }, 1, 0],
+            },
+          },
+          tn: {
+            $sum: {
+              $cond: [{ $eq: ['$pemilih.id_category', '8'] }, 1, 0],
+            },
+          },
+        },
+      },
+
+      {
+        $project: {
+          _id: 0,
+          anggota: 1,
+          dpc: 1,
+          dpra: 1,
+          bko: 1,
+          bpkk: 1,
+          bpu: 1,
+          kepemudaan: 1,
+          tn: 1,
         },
       },
     ]);
+
+    return data;
+
+    // return this.suara.aggregate([
+    //   // {
+    //   //   $group: {
+    //   //     _id: '$id_dpr_level',
+    //   //     count: { $sum: 1 },
+    //   //   },
+    //   // },
+    //   // {
+    //   //   $lookup: {
+    //   //     from: 'pemilihs',
+    //   //     localField: 'user_id',
+    //   //     foreignField: '_id',
+    //   //     as: 'pemilih',
+    //   //   },
+    //   // },
+    //   // {
+    //   //   $unwind: {
+    //   //     path: '$pemilih',
+    //   //     preserveNullAndEmptyArrays: true,
+    //   //   },
+    //   // },
+    //   {
+    //     $match: filter,
+    //   },
+    // ]);
   }
 }
